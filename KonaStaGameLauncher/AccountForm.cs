903219bf9e8eb26f856510f33f6bf1efd615d2b3
anon <anon@anoncom.net>
2021-11-21 04:59:18 +0900

    Account manager form

---
+++ KonaStaGameLauncher/AccountForm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Security;
+using System.Windows.Forms;
+using AdysTech.CredentialManager;
+
+namespace KonaStaGameLauncher
+{
+    public partial class AccountForm : Form
+    {
+        private readonly string CredentialName = Properties.Resources.CredentialTarget;
+
+        NetworkCredential credential;
+
+        public AccountForm()
+        {
+            InitializeComponent();
+            button_Update.Text = Resources.ButtonUpdate;
+            button_Remove.Text = Resources.ButtonRemove;
+            button_Close.Text = Resources.ButtonClose;
+        }
+
+        ~AccountForm()
+        {
+            credential = null;
+        }
+
+        private void AccountForm_Load(object sender, EventArgs e)
+        {
+            button_Update.Text = Resources.ButtonUpdate_Register;
+            button_Remove.Enabled = false;
+            if (!RefreshRegisteredAccounts())
+            {
+                // There are no account registered
+                // Display credential input prompt
+                bool save = true;
+                credential = CredentialManager.PromptForCredentials(CredentialName, ref save, 
+                    Resources.EnterYourAccountPasswordPrompt, Resources.AppName, "");
+                if (credential != null)
+                {
+                    credential.Domain = Properties.Resources.CredentialDomain;
+                    CredentialManager.SaveCredentials(CredentialName, credential);
+                }
+                RefreshRegisteredAccounts();
+            }
+        }
+
+        private void Button_Remove_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(Resources.ConfirmToRemoveAccountFromList, Resources.ConfirmToRemove, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    if (CredentialManager.RemoveCredentials(CredentialName))
+                        MessageBox.Show(Resources.AccountRemoveSucceeded);
+                    else
+                    {
+                        result = MessageBox.Show(Resources.AccountRemoveFailed, Resources.AppName, MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                        if (result == DialogResult.Retry)
+                        {
+                            Utils.Common.OpenControlPanel("keymgr.dll,KRShowKeyMgr");
+                        }
+                    }
+                }
+                catch (CredentialAPIException)
+                {
+                    result = MessageBox.Show(Resources.AccountRemoveFailed, Resources.AppName, MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                    if (result == DialogResult.Retry)
+                    {
+                        Utils.Common.OpenControlPanel("keymgr.dll,KRShowKeyMgr");
+                    }
+                }
+            }
+        }
+
+        private void Button_Update_Click(object sender, EventArgs e)
+        {
+            bool save = true;
+            string defaultUserName = "";
+            if (credential != null)
+                defaultUserName = credential.UserName;
+
+            credential = CredentialManager.PromptForCredentials(CredentialName, ref save, Resources.EnterYourAccountPasswordPrompt, Resources.AppName, defaultUserName);
+            if (credential != null)
+                CredentialManager.SaveCredentials(CredentialName, credential);
+            if (save)
+            {
+                RefreshRegisteredAccounts();
+            }
+        }
+
+        private void Button_Close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private bool RefreshRegisteredAccounts()
+        {
+            label_UserAccountID_Value.Text = Resources.NoAccountRegistered;
+            button_Update.Text = Resources.ButtonUpdate_Register;
+            button_Remove.Enabled = false;
+            List<NetworkCredential> list = CredentialManager.EnumerateCredentials(CredentialName);
+            if (list != null && list.Count > 0)
+            {
+                list.ForEach(credential =>
+                {
+                    this.credential = credential;
+                    label_UserAccountID_Value.Text = credential.UserName;
+                });
+                button_Remove.Enabled = true;
+                button_Update.Text = Resources.ButtonUpdate;
+            }
+            return list != null && list.Count > 0;
+        }
+    }
+}
